761f7404d60cb98d57368e190cc5cfa4e2c7d7c3
manhcuong130295 <68835416+manhcuong130295@users.noreply.github.com>
2020-09-23 09:35:18 +0700

    up bai

--- src/_11_generic_stack_queue/bai_tap/dao_nguoc_phan_tu/ReverseString.java
+++ src/_11_generic_stack_queue/bai_tap/dao_nguoc_phan_tu/ReverseString.java
@@ -1,14 +1,24 @@
 package _11_generic_stack_queue.bai_tap.dao_nguoc_phan_tu;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class ReverseString {
     public static void main(String[] args) {
-     Scanner scanner = new Scanner(System.in);
-     String[] mWord = scanner.nextLine().split(",");
-     for (int i =0; i<mWord.length;i++) {
-         
-     }
+        Stack<String> stringStack = new Stack<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter String");
+        String[] mWord = scanner.nextLine().split(" ");
+        for (int i = 0; i < mWord.length; i++) {
+            stringStack.push(mWord[i]);
+        }
+        Stack<String> stringStackTemp=new Stack<>();
+        System.out.println("before" + stringStack);
+        while (!stringStack.isEmpty()) {
+            stringStackTemp.push(stringStack.pop());
+        }
+        stringStack=stringStackTemp;
+        System.out.println("After reverse"+stringStack);
     }
 }
