9d2ba13b989847ce61f0fb3413d4b2d36636dad6
manhcuong130295 <68835416+manhcuong130295@users.noreply.github.com>
2020-10-02 09:11:32 +0700

    up bai

--- src/_16_io_file_text/bai_tap/CopyFile.java
+++ src/_16_io_file_text/bai_tap/CopyFile.java
@@ -6,19 +6,23 @@ import java.util.List;
 
 public class CopyFile {
 
+    public static final String FILE_PATH_STUDENT1 = "D:\\ChuongTrinhCodegym\\Module2_C0720G1_Le_Manh_Cuong\\src\\_16_io_file_text\\bai_tap\\Student1";
+    public static final String REGEX = ",";
+    public static final String FILE_PATH_STUDENT = "D:\\ChuongTrinhCodegym\\Module2_C0720G1_Le_Manh_Cuong\\src\\_16_io_file_text\\bai_tap\\Student.csv";
+
     public static List<Student> copy() {
         List<Student> studentList = new ArrayList<>();
         try {
-            FileReader fileReader = new FileReader("D:\\ChuongTrinhCodegym\\Module2_C0720G1_Le_Manh_Cuong\\src\\_16_io_file_text\\bai_tap\\Student.csv");
+            FileReader fileReader = new FileReader(FILE_PATH_STUDENT);
             BufferedReader bufReadFile = new BufferedReader(fileReader);
             String line;
             String[] temp;
             Student student;
             while ((line=bufReadFile.readLine())!=null){
-                temp = line.split(",");
+                temp = line.split(REGEX);
                 student = new Student(Integer.parseInt(temp[0]),temp[1]);
                 studentList.add(student);
-                FileWriter fileWriter = new FileWriter("D:\\ChuongTrinhCodegym\\Module2_C0720G1_Le_Manh_Cuong\\src\\_16_io_file_text\\bai_tap\\Student1",true);
+                FileWriter fileWriter = new FileWriter(FILE_PATH_STUDENT1,true);
                 BufferedWriter bufferedWriter= new BufferedWriter(fileWriter);
                 bufferedWriter.write(line+"\n");
                 bufferedWriter.close();
